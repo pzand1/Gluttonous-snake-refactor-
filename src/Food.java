215bba2f5588142481b9af7e902e2c5a43e202d4
‘pzand’ <pzand17875626301@163.com>
2022-10-11 22:17:34 +0800

    算法绘图库上传

--- src/Food.java
+++ src/Food.java
@@ -1,3 +1,24 @@
-public class Food extends Entity{
+import java.util.Random;
 
+public class Food extends Entity{
+    protected void createFood(Snake[] snakes){
+        Random random = new Random();
+        boolean temp = true;
+        while(temp){
+            temp = false;
+            this.x = random.nextInt();
+            this.y = random.nextInt();
+            for (Snake snake:snakes) {
+                for (Entity body : snake.getBody()) {
+                    if(!this.circular(body)){
+                        temp = true;
+                        break;
+                    }
+                }
+                if(temp){
+                    break;
+                }
+            }
+        }
+    }
 }
